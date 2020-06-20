35029925d0fb3e8a070710cd00962b179de54d0f
TaimoorSalyhal <asfandtaimoor786@gmail.com>
2020-06-20 14:28:43 +0500

    Fixed some performance issue and adding different images for different section of page

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 // Show the navigation bar
 
 $(".fa-bars").click(function() {
-    $("#navigation-bar").show();
-
-    $(".fa-bars").hide(); // hide menu icon
-    $(".fa-times").show(); // Show the exit icon
-
+    $(".nav-pills").toggle();
 });
 
 
-$(".fa-times").click(function() {
-    $("#navigation-bar").hide();
-
-    $(".fa-bars").show(); // hide menu icon
-    $(".fa-times").hide(); // Show the exit icon
-
+// Hide the navBar when click on times icon and click on the link
+$(".nav-link").click(function() {
+    $(".nav-pills").hide();
 });
